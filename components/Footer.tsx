ac64ce083cea096cf31930c91d7ee3c841100c05
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2024-10-16 07:31:36 +0530

    build it

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,48 +8,76 @@ const Footer = () => {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href="/" >
-            <Image src="/logo/transbg.png" alt="logo" width={300} height={150} />
+          <Link href="/">
+            <Image
+              src="/logo/transbg.png"
+              alt="logo"
+              width={300}
+              height={150}
+            />
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
-                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
-                      {link}
-                    </Link>
-                  ))}
-                </ul>
-              </FooterColumn>
-            ))}
+            {FOOTER_LINKS.map(
+              (
+                columns,
+                index // Added unique key here
+              ) => (
+                <FooterColumn
+                  title={columns.title}
+                  key={`footer-column-${index}`}
+                >
+                  <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                    {columns.links.map(
+                      (
+                        link,
+                        linkIndex // Added unique key here
+                      ) => (
+                        <Link href="/" key={`footer-link-${linkIndex}`}>
+                          {link}
+                        </Link>
+                      )
+                    )}
+                  </ul>
+                </FooterColumn>
+              )
+            )}
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
-                  <Link
-                    href="/"
-                    key={link.label}
-                    className="flex gap-4 md:flex-col lg:flex-row"
-                  >
-                    <p className="whitespace-nowrap">{link.label}:</p>
-                    <p className="medium-14 whitespace-nowrap text-blue-70">
-                      {link.value}
-                    </p>
-                  </Link>
-                ))}
+                {FOOTER_CONTACT_INFO.links.map(
+                  (
+                    link,
+                    contactIndex // Added unique key here
+                  ) => (
+                    <Link
+                      href="/"
+                      key={`contact-info-${contactIndex}`}
+                      className="flex gap-4 md:flex-col lg:flex-row"
+                    >
+                      <p className="whitespace-nowrap">{link.label}:</p>
+                      <p className="medium-14 whitespace-nowrap text-blue-70">
+                        {link.value}
+                      </p>
+                    </Link>
+                  )
+                )}
               </FooterColumn>
             </div>
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={SOCIALS.title}>
                 <ul className="regular-14 flex gap-4 text-gray-30">
-                  {SOCIALS.links.map((link) => (
-                    <Link href="/" key={link}>
-                      <Image src={link} alt="logo" width={24} height={24} />
-                    </Link>
-                  ))}
+                  {SOCIALS.links.map(
+                    (
+                      link,
+                      socialIndex // Added unique key here
+                    ) => (
+                      <Link href="/" key={`social-link-${socialIndex}`}>
+                        <Image src={link} alt="logo" width={24} height={24} />
+                      </Link>
+                    )
+                  )}
                 </ul>
               </FooterColumn>
             </div>
