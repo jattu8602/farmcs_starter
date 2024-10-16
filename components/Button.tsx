aa9977e886d6be4af278eb4020ca6b832053d208
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2024-10-16 08:03:32 +0530

    hiii

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,15 +6,23 @@ type ButtonProps = {
   icon?: string
   variant: string
   full?: boolean
+  className?: string // Added className prop
 }
 
-const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+const Button = ({
+  type,
+  title,
+  icon,
+  variant,
+  full,
+  className,
+}: ButtonProps) => {
   return (
     <button
       type={type}
       className={`flexCenter gap-3 rounded-full border ${variant} ${
         full ? 'w-full' : ''
-      }`}
+      } ${className}`} // Apply the passed className
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <span className="bold-16 whitespace-nowrap cursor-pointer">{title}</span>
