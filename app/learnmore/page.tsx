cc10558ae8efcc629d1419432329ac8c2462f804
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2024-11-17 22:11:03 +0530

    new changes

--- app/learnmore/page.tsx
+++ app/learnmore/page.tsx
@@ -1,11 +1,19 @@
 import Intro from '@/components/Intro'
 import Swiper from '@/components/Swiper'
 import Tech from '@/components/Tech'
+import Image from 'next/image'
+
 export default function LearnMore() {
   return (
     <>
       <Intro />
-      <Swiper/>
+      <Swiper />
+      <Image
+        src="/farmcs/workflow.png"
+        alt="workflow"
+        width={1800}
+        height={1200}
+        className='md:mt-[-100px] mt-3 '/>
       <Tech/>
     </>
   )
